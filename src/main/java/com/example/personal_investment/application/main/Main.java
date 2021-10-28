f1bac603f1be2ea431457307a050b0b54a0a2c02
Dsribass <ds_ribas@hotmail.com>
2021-10-28 11:16:45 -0300

    feat: setup application

---
+++ src/main/java/com/example/personal_investment/application/main/Main.java
@@ -0,0 +1,9 @@
+package com.example.personal_investment.application.main;
+
+import com.example.personal_investment.application.view.HelloApplication;
+
+public class Main {
+    public static void main(String[] args) {
+        HelloApplication.main(args);
+    }
+}
