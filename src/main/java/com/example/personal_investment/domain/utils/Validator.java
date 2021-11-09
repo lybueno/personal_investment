d9da9948d0d1c9d39c34b636d7ac4d3387cac1ef
Daniel de Souza Ribas <danielribas17@gmail.com>
2021-11-09 10:11:46 -0300

    refactor: some changes in Validator

--- src/main/java/com/example/personal_investment/domain/utils/Validator.java
+++ src/main/java/com/example/personal_investment/domain/utils/Validator.java
@@ -1,21 +1,26 @@
 package com.example.personal_investment.domain.utils;
 
 import com.example.personal_investment.domain.entities.stock.Stock;
+import com.example.personal_investment.domain.entities.stock_transaction.StockTransaction;
 import com.example.personal_investment.domain.entities.wallet.Wallet;
+import com.example.personal_investment.domain.exceptions.TypeNotMatchException;
 
 public class Validator {
     public static void validateStock(Stock stock) {
+        if(stock.getId() == null){
+            throw new IllegalArgumentException("Stock id cannot be null");
+        }
         if(stock.getType() == null){
-            throw new IllegalArgumentException("Stock can not be null");
+            throw new IllegalArgumentException("Stock type cannot be null");
         }
         if(stock.getTicker() == null){
-            throw new IllegalArgumentException("Ticker cannot be null ");
+            throw new IllegalArgumentException("Stock Ticker cannot be null ");
         }
         if(stock.getCnpj() == null){
-            throw new IllegalArgumentException("CNPJ cannot be null ");
+            throw new IllegalArgumentException("Stock CNPJ cannot be null ");
         }
         if(stock.getCompanyName() == null){
-            throw new IllegalArgumentException("Company name cannot be null ");
+            throw new IllegalArgumentException("Stock Company name cannot be null ");
         }
     }
     public static void validateWallet(Wallet wallet){
@@ -25,6 +30,31 @@ public class Validator {
 
         if(wallet.getName() == null){
             throw new IllegalArgumentException("Wallet name cannot be null");
+        }
+    }
+
+    public static void validateTransaction(StockTransaction transaction) {
+        if (transaction.getStock() == null) {
+            throw new IllegalArgumentException("Stock cannot be null");
+        }
+        if (transaction.getWallet() == null) {
+            throw new IllegalArgumentException("Wallet cannot be null");
+        }
+        if (transaction.getTransactionDate() == null) {
+            throw new IllegalArgumentException("Transaction Date cannot be null");
+        }
+        if (transaction.getUnitaryValue() == null) {
+            throw new IllegalArgumentException("Stock unitary value cannot be null");
+        }
+        if (transaction.getTransactionType() == null) {
+            throw new IllegalArgumentException("Transaction Type cannot be null");
+        }
+
+        Validator.validateStock(transaction.getStock());
+        Validator.validateWallet(transaction.getWallet());
+
+        if (transaction.getStock().getType() != transaction.getWallet().getType()) {
+            throw new TypeNotMatchException("The stock type does not match with wallet type");
         }
     }
 }
