461f2442e7bab57be9d5f6b60140f22b54ea6fb3
Daniel Ribas <danielribas17@gmail.com>
2021-11-28 19:00:12 -0300

    add stock management controller

---
+++ src/main/java/com/example/personal_investment/application/controllers/StockManagementController.java
@@ -0,0 +1,37 @@
+package com.example.personal_investment.application.controllers;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+
+public class StockManagementController {
+    @FXML
+    public TextField tfSearchStock;
+    @FXML
+    public Label username;
+
+    public void addStock(ActionEvent actionEvent) {
+    }
+
+    public void updateStock(ActionEvent actionEvent) {
+    }
+
+    public void deleteStock(ActionEvent actionEvent) {
+    }
+
+    public void searchStock(ActionEvent actionEvent) {
+    }
+
+    public void registerPurchaseStock(ActionEvent actionEvent) {
+    }
+
+    public void reportsPage(ActionEvent actionEvent) {
+    }
+
+    public void walletPage(ActionEvent actionEvent) {
+    }
+
+    public void logout(ActionEvent actionEvent) {
+    }
+}
