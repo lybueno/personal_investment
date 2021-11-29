6f9e39f9c9ac4b2d727c25591797e01159b48b55
Daniel de Souza Ribas <danielribas17@gmail.com>
2021-11-29 10:58:29 -0300

    Changes by mylla

--- src/main/java/com/example/personal_investment/application/common/Routes.java
+++ src/main/java/com/example/personal_investment/application/common/Routes.java
@@ -6,4 +6,8 @@ public class Routes {
     public static final String investmentPage = "investments-view";
     public static final String stockManagementPage = "stock-management-view";
     public static final String reportPage = "reports-view";
+    public static final String walletPage = "wallet-view";
+    public static final String brokerageNotePage = "brokerage-note-view";
+    public static final String darfPage = "darf-view";
+    public static final String incomeTaxPage = "income-tax-view";
 }
