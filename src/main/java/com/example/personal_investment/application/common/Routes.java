5f4fc8ed797bd1774970969a81f90059f82e7b20
Myllena Graciani <myllenagraciani@gmail.com>
2021-11-29 11:53:52 -0300

    add rota de add stock

--- src/main/java/com/example/personal_investment/application/common/Routes.java
+++ src/main/java/com/example/personal_investment/application/common/Routes.java
@@ -7,7 +7,5 @@ public class Routes {
     public static final String stockManagementPage = "stock-management-view";
     public static final String reportPage = "reports-view";
     public static final String walletPage = "wallet-view";
-    public static final String brokerageNotePage = "brokerage-note-view";
-    public static final String darfPage = "darf-view";
-    public static final String incomeTaxPage = "income-tax-view";
+    public static final String addStockPage = "add-stock-view";
 }
