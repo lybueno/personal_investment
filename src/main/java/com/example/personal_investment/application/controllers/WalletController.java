ccecfae0d81302fd456591504ebf7723dd74e1d8
renan.oliveira <renan.oliveira.rao@gmail.com>
2021-11-30 19:26:18 -0300

    Add controller wallet

---
+++ src/main/java/com/example/personal_investment/application/controllers/WalletController.java
@@ -0,0 +1,26 @@
+package com.example.personal_investment.application.controllers;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+
+public class WalletController {
+
+    public void addWallet(ActionEvent actionEvent) {
+    }
+
+    public void updateWallet(ActionEvent actionEvent) {
+    }
+
+    public void deleteWallet(ActionEvent actionEvent) {
+    }
+
+    public void backPreviousScreen(ActionEvent actionEvent) {
+    }
+
+    public void seeWallet(ActionEvent actionEvent) {
+    }
+
+    public void logout(ActionEvent actionEvent) {
+    }
+}
