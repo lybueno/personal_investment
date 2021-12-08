213d5aab51ef187727b568dffe01d8dfa17f0808
renan.oliveira <renan.oliveira.rao@gmail.com>
2021-12-08 10:59:07 -0300

    campo de senha no login e arrumamos a confirmação de da venda de uma ação

--- src/main/java/com/example/personal_investment/application/controllers/LoginController.java
+++ src/main/java/com/example/personal_investment/application/controllers/LoginController.java
@@ -10,6 +10,7 @@ import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
 
 import java.io.IOException;
@@ -23,7 +24,7 @@ public class LoginController {
     @FXML
     private TextField txtUserName;
     @FXML
-    private TextField txtPassword;
+    private PasswordField txtPassword;
     @FXML
     private Button btnLogin;
     @FXML
